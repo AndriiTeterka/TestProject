c5fb8314ce8069b36dc5d67404e5a4514286bba3
Andrii Teterka <andrii.teterka@pluto.tv>
2023-02-14 14:27:57 +0200

    testProject

---
+++ src/main/java/API/Specifications.java
@@ -0,0 +1,27 @@
+package API;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class Specifications {
+
+    public static RequestSpecification requestSpecification() {
+        return new RequestSpecBuilder().setContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification responseSpecification() {
+        return new ResponseSpecBuilder().expectStatusCode(200)
+                .build();
+    }
+
+    public static void specInstall(RequestSpecification requestSpecification, ResponseSpecification responseSpecification) {
+        RestAssured.requestSpecification = requestSpecification;
+        RestAssured.responseSpecification = responseSpecification;
+    }
+}
---
+++ src/test/java/API/apiTest.java
@@ -0,0 +1,28 @@
+package API;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+
+public class apiTest {
+
+    private final static String URL = "https://reqres.in/api/users?page=2";
+
+    @Test
+    public void testUsers() {
+        Specifications.specInstall(Specifications.requestSpecification(), Specifications.responseSpecification());
+        List<UserData> users = given().when()
+                .get(URL)
+                .then().log().all()
+                .extract().body().jsonPath()
+                .getList("data", UserData.class);
+
+        users.stream().filter(x -> x.getId().equals(10)).forEach(b -> {
+            Assert.assertEquals(b.getFirst_name(), "Byron");
+            Assert.assertEquals(b.getLast_name(), "Fields");
+        });
+    }
+}
